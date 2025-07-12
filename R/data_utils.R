f4fd6951a705372fed55f942db2291c0d33a4502
sethbassetti <sethbassetti@gmail.com>
2025-07-12 11:30:37 -0700

    Finished tasks 4 and 5

--- R/data_utils.R
+++ R/data_utils.R
@@ -34,9 +34,7 @@ clean_df <- function(df) {
       Month = lubridate::month(Date),
       Year = lubridate::year(Date),
       Day = lubridate::day(Date)
-    ) |>
-    # We don't need the date column anymore
-    select(!Date)
+    )
 
   return(clean_df)
 }
@@ -48,8 +46,45 @@ compute_average_price <- function(df) {
   average_df <- df |>
     dplyr::group_by(Year, Month, SettlementPoint) |>
     dplyr::summarize(AveragePrice = mean(Price), .groups = "drop") |>
+
     # Reorder the columns to work with CSV format
     dplyr::relocate(c(SettlementPoint, Year, Month, AveragePrice))
 
   return(average_df)
+}
+
+#' Computes the hourly volatility for each settlement point and year
+#'
+#' Taking in a tibble dataframe of hourly power prices, this function calculates the volatility
+#' for each settlement hub over each year. Hourly volatility is defined as the standard deviation
+#' of the log returns, which is calculated below
+#'
+#' @param df The dataframe containing the power data
+#' @returns A dataframe of the shape SettlementPoint x Year x HourlyVolatility
+compute_hourly_volatility <- function(df) {
+  hourly_volatility_df <- df |>
+    # This will cause a 1 hour gap in some of the data points, however it is infrequent
+    # enough that it should not cause long term issues
+    dplyr::filter(stringr::str_starts(SettlementPoint, "HB") & Price > 0) |>
+
+    # Make sure data is arranged in order of date for this to work
+    dplyr::arrange(Date) |>
+
+    # Group into each hub and calculate the log returns
+    dplyr::group_by(SettlementPoint) |>
+    dplyr::mutate(
+      LagPrice = dplyr::lag(Price),
+      Return = Price / LagPrice,
+      LogReturn = log(Return)
+    ) |>
+
+    # Finally, group by the variables of interest and calculate the volatility
+    dplyr::group_by(Year, SettlementPoint) |>
+    dplyr::summarize(
+      HourlyVolatility = sd(LogReturn, na.rm = TRUE),
+      .groups = "drop"
+    ) |>
+    dplyr::relocate(SettlementPoint, Year, HourlyVolatility)
+
+  return(hourly_volatility_df)
 }
--- main.R
+++ main.R
@@ -6,12 +6,12 @@ library(dplyr)
 library(janitor)
 library(stringr)
 library(readr)
+library(conflicted)
 
 
 # Task 1: Read Data ------------------------------------------------------
 
 power_df <- read_files("data")
-
 
 # Clean Data -------------------------------------------------------------
 clean_df <- clean_df(power_df)
@@ -24,3 +24,15 @@ average_price_df <- compute_average_price(clean_df)
 # Task 3: Write CSV to Disk ----------------------------------------------
 
 readr::write_csv(average_price_df, "output/AveragePriceByMonth.csv")
+
+
+# Task 4: Hourly Volatility ----------------------------------------------
+
+hourly_volatility_df <- compute_hourly_volatility(clean_df)
+
+
+# Task 5: Write Volatility to Disk ---------------------------------------
+
+readr::write_csv(hourly_volatility_df, "output/HourlyVolatilityByYear.csv")
+
+# Task 6: Determine Highest Volatility -----------------------------------
